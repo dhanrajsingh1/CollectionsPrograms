89f37246063d4e7c7d0bf94b76c842939c9f91d4
dhanrajsingh1 <raj31852@gmail.com>
2024-12-23 14:35:18 +0530

    ReversingElements

---
+++ ReversingElements.java
@@ -0,0 +1,22 @@
+package CollectionsPrograms;
+
+import java.util.ArrayList;
+
+public class ReversingElements {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
+
+        for(int i=0, j=al.size()-1; i<al.size()/2; i++,j--){
+            int temp = al.get(i);
+            al.set(i, al.get(j));
+            al.set(j, temp);
+        }
+        System.out.println(al);
+        
+    }
+}
